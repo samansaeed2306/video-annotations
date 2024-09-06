68c9c995adfe3dcdaed664929b6c31dbf511ee7d
Saman Saeed <samansaeed2306@gmail.com>
2024-09-06 12:49:19 +0500

    Update libray.js to add options

--- public/js/library.js
+++ public/js/library.js
@@ -31,7 +31,17 @@ function addVideoCard(videoSrc, title) {
     const heading = document.createElement('h3');
     heading.textContent = title;
 
+    const editButton = document.createElement('button');
+    editButton.className = 'edit-button';
+    const icon = document.createElement('img');
+    icon.src = '../icons/pencil.png';  
+    icon.alt = 'Edit';
+    editButton.appendChild(icon);
+
+    // Append heading and button to the card content
     cardContent.appendChild(heading);
+    cardContent.appendChild(editButton);
+
     card.appendChild(video);
     card.appendChild(cardContent);
     gallery.appendChild(card);
