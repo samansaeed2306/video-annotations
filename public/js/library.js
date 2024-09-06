1f21c916afab88a39ed5be91a52812cdf501df22
Saman Saeed <samansaeed2306@gmail.com>
2024-09-06 18:01:54 +0500

    Add Image panel to library

--- public/js/library.js
+++ public/js/library.js
@@ -64,3 +64,55 @@ function loadVideo(manifestUri) {
 }
 addVideoCard('../sampleVideos/rolling-tissues.mp4', 'rolling-tissues.mp4');
 addVideoCard('../sampleVideos/burning-planet.mp4', 'burning-planet.mp4');
+
+
+const imageGallery = document.getElementById('imageGallery');
+const imageFileInput = document.getElementById('imageFileInput');
+const loadMoreImages = document.getElementById('loadMoreImages');
+
+loadMoreImages.addEventListener('click', function(e) {
+    e.preventDefault();
+    imageFileInput.click();
+});
+
+imageFileInput.addEventListener('change', function(e) {
+    const file = e.target.files[0];
+    if (file) {
+        const imageURL = URL.createObjectURL(file);
+        addImageCard(imageURL, file.name);
+    }
+});
+
+function addImageCard(imageSrc, title) {
+    const card = document.createElement('div');
+    card.className = 'card';
+
+    const img = document.createElement('img');
+    img.src = imageSrc;
+    img.alt = title;
+
+    const cardContent = document.createElement('div');
+    cardContent.className = 'card-content';
+
+    const heading = document.createElement('h3');
+    heading.textContent = title;
+    
+    const editButton = document.createElement('button');
+    editButton.className = 'edit-button';
+    editButton.id = 'edit-button-img';
+    const icon = document.createElement('img');
+    icon.src = '../icons/pencil.png';  
+    icon.alt = 'Edit';
+    editButton.appendChild(icon);
+
+
+    cardContent.appendChild(heading);
+    cardContent.appendChild(editButton);
+
+    card.appendChild(img);
+    card.appendChild(cardContent);
+    imageGallery.appendChild(card);
+}
+
+addImageCard('../sampleImages/dolphin.jfif', 'Dolphin');
+// addImageCard('../sampleImages/image2.jpg', 'Image 2');
