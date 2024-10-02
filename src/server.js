13feef8bab96c8d4ffe6a19e84bd114001d50f7e
Saman Saeed <samansaeed2306@gmail.com>
2024-10-02 16:00:41 +0500

    Add etags for performance optimization

--- src/server.js
+++ src/server.js
@@ -24,9 +24,13 @@ app.use(cors({
   credentials: true
 }));
 
+app.use(express.static(path.join(__dirname, '../public'), {
+  etag: true,          // Enable ETag generation (enabled by default)
+  maxAge: '1d'         // Cache files for 1 day (adjust as needed)
+}));
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, '../public')));
+// app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../public/uploads')));
 
 connectToDb();
