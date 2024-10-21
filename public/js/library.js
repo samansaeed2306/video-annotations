f9fc706f0e0d8f883f52bab1a2ebb0c8bc08bef9
Saman Saeed <samansaeed2306@gmail.com>
2024-10-21 15:45:54 +0500

    Call refreshVideoGallery soon after getting data

--- public/js/library.js
+++ public/js/library.js
@@ -62,7 +62,7 @@ fileInput.addEventListener('change', function(e) {
         .then(response => response.json())
         .then(data => {
             console.log('Video uploaded:', data);
-            // refreshVideoGallery();
+            refreshVideoGallery();
             // Optionally update the video src with the new path
             // video.src = `/uploads/${data.media.fileName}`;
             console.log('Video originalName:', data.media.originalName);
