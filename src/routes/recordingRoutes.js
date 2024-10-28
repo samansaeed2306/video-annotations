b999124cbe552f64e64ca65dbf13c0d453e0ae1b
Saman Saeed <samansaeed2306@gmail.com>
2024-10-28 12:08:47 +0500

    Serve the static files to get the video file

--- src/routes/recordingRoutes.js
+++ src/routes/recordingRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import multer from 'multer';
-import { uploadRecording, getRecordingsByUserId } from '../controllers/recordingController.js';
+import { uploadRecording, getRecordingsByUserId, getAllRecordings } from '../controllers/recordingController.js';
 
 const router = express.Router();
 const upload = multer({ dest: 'recordings/' });
@@ -11,4 +11,5 @@ router.post('/recordings/:userId', upload.single('video'), uploadRecording);
 
 router.get('/recordings/user/:userId', getRecordingsByUserId);
 
+router.get('/recordings/getAll', getAllRecordings);
 export default router;
