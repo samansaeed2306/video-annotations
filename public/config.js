55c786608f414a2286fa1a56d1f1f8fa3a0d2052
Saman Saeed <samansaeed2306@gmail.com>
2025-01-02 14:09:32 +0500

    Update config.js and .env

--- public/config.js
+++ public/config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
-    // API_REC_URL: 'http://localhost:8080/api/rec/',
-    // API_MED_URL:'http://localhost:8080/api/media',
-    // API_URL: 'http://localhost:8080/api',
-    API_REC_URL: 'http://192.168.100.191:8080/api/rec/',
-    API_MED_URL:'http://192.168.100.191:8080/api/media',
-    API_URL: 'http://192.168.100.191:8080/api',
+    API_REC_URL: 'http://localhost:8080/api/rec/',
+    API_MED_URL:'http://localhost:8080/api/media',
+    API_URL: 'http://localhost:8080/api',
+    // API_REC_URL: 'http://192.168.100.191:8080/api/rec/',
+    // API_MED_URL:'http://192.168.100.191:8080/api/media',
+    // API_URL: 'http://192.168.100.191:8080/api',
 };
