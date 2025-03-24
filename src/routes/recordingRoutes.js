b69f5006604e3c27af8d77959bd36e6edd8f9639
Saman Saeed <samansaeed2306@gmail.com>
2025-03-24 14:52:01 +0500

    Update recordings route

--- src/routes/recordingRoutes.js
+++ src/routes/recordingRoutes.js
@@ -3,7 +3,19 @@ import multer from 'multer';
 import { uploadRecording, getRecordingsByUserId, getAllRecordings } from '../controllers/recordingController.js';
 
 const router = express.Router();
-const upload = multer({ dest: 'recordings/' });
+// const upload = multer({ dest: 'recordings/' });
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'recordings/');
+    },
+    filename: (req, file, cb) => {
+        const fileExtension = path.extname(file.originalname); // Get the file extension
+        const uniqueName = `${Date.now()}-${Math.round(Math.random() * 1E9)}${fileExtension}`;
+        cb(null, uniqueName);
+    }
+});
+
+const upload = multer({ storage: storage });
 
 
 router.post('/recordings/:userId', upload.single('video'), uploadRecording);
