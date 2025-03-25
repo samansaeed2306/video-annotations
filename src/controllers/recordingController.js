ab0e7a676eb07493d03d102e3296375d1be7c903
Saman Saeed <samansaeed2306@gmail.com>
2025-03-25 16:02:32 +0500

    Update recording title to add extension

--- src/controllers/recordingController.js
+++ src/controllers/recordingController.js
@@ -33,7 +33,7 @@ export const uploadRecording = async (req, res) => {
         let title = 'Recording 1'; 
         if (lastRecording) {
             const lastTitleNumber = parseInt(lastRecording.title.split(' ')[1]); 
-            title = `Recording ${lastTitleNumber + 1}`; 
+            title = `Recording ${lastTitleNumber + 1}.webm`; 
         }
 
        
